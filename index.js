a6d5bcfbfa67a52c9c6865e55fa830bacde13bb9
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-04-01 14:02:06 +0200

    Renewed Account login function

---
+++ index.js
@@ -0,0 +1,6 @@
+async function connectWallet() {
+    accounts = await window.ethereum.request({method: "eth_requestAccounts"}).catch((err) => {
+        console.log(err.code)
+    })
+    console.log(accounts)
+}

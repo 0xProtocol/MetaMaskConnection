c77a2bb42e4dd0ded17e6a30061ab52c6d2d64ba
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-04-02 14:48:41 +0200

    Check Balance in logging

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ async function connectWallet() {
     })
     console.log(accounts)
 }
+
+async function checkBalance() {
+    let balance = await window.ethereum.request({
+        method: "eth_getBalance",
+        params: [
+            "0xD585ef6eA45EeAe19b4454B1dB15B696FcbE94dD",
+            'latest'
+
+        ]
+    }).catch((err) => {
+        console.log(err.code)
+    })
+    console.log(parseInt(balance) / Math.pow(10,18))
+}
